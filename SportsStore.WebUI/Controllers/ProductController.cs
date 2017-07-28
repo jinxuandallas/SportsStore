c7a003e6d0dc08bbec0c43e0e3fb5e7464ddf632
Dallas <dallas2k@163.com>
2017-07-28 22:02:02 +0800

    终于解决数据库782问题

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -19,6 +19,7 @@ namespace SportsStore.WebUI.Controllers
         
         public ViewResult List(int page=1)
         {
+            //return View(repository.Products); 
             return View(repository.Products.OrderBy(p=>p.ProductID).Skip((page-1)*PageSize).Take(PageSize));
         }
     }
