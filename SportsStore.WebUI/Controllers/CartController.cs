b5eef454a95ea43c4e3eb3dc54a6a70f44828761
Dallas <dallas2k@163.com>
2017-08-04 16:17:31 +0800

    2017/8/4 9.1

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -13,33 +13,34 @@ namespace SportsStore.WebUI.Controllers {
             repository = repo;
         }
 
-        public ViewResult Index(string returnUrl) {
+        public ViewResult Index(Cart cart, string returnUrl) {
             return View(new CartIndexViewModel {
-                Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
 
-        public RedirectToRouteResult AddToCart(int productId, string returnUrl) {
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl) {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null) {
-                GetCart().AddItem(product, 1);
+                cart.AddItem(product, 1);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl) {
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl) {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null) {
-                GetCart().RemoveLine(product);
+                cart.RemoveLine(product);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
+        /*
         private Cart GetCart() {
 
             Cart cart = (Cart)Session["Cart"];
@@ -49,5 +50,6 @@ namespace SportsStore.WebUI.Controllers {
             }
             return cart;
         }
+        */
     }
 }
