bbad9225ec84f01ecef661a80900a1e9722a19c6
Dallas <dallas2k@163.com>
2017-08-11 17:02:37 +0800

    2017\8\11

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web.Mvc;
 using Moq;
@@ -39,6 +40,13 @@ namespace SportsStore.WebUI.Infrastructure {
             kernel.Bind<IProductRepository>().ToConstant(mock.Object);
             */
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
         }
     }
 }
